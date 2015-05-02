21d922dd53504a1debea9cb2bf65f8dd85a9867a
Gene Liverman <gliverma@westga.edu>
2015-05-01 22:46:09 -0400

    Added config and redis connection settings

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
+var config       = require('config');
+var dgram        = require('dgram');
+var redis        = require('redis');
 var syslogParser = require('glossy').Parse;
-var dgram = require("dgram");
-var socket = dgram.createSocket("udp4");
+
+var redis_host   = config.get('sawyer.redis_host');
+var redis_port   = config.get('sawyer.redis_port');
+var redis_pass   = config.get('sawyer.redis_password');
+
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
+
+// connection using a password if one was provided
+if (isEmpty(redis_pass)) {
+  var redis_client = redis.createClient(redis_port, redis_host, {});
+} else {
+  var redis_client = redis.createClient(redis_port, redis_host, {auth_pass: redis_pass});
+}
+
+var socket       = dgram.createSocket('udp4');
+
+redis_client.on('connect', function() {
+    console.log('connected to Redis on ' + redis_host + ':' + redis_port);
+});
 
 socket.on("error", function (err) {
   console.log("server error:\n" + err.stack);
