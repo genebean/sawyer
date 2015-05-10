82d1732b4e07a586395f56c565e1f0818653aaa7
Gene Liverman <gliverma@westga.edu>
2015-05-09 23:08:39 -0400

    started stubbing out tests

---
+++ test/serverSpec.js
@@ -0,0 +1,13 @@
+var expect = require("chai").expect;
+var sawyer = require("../server.js");
+
+describe("Sawyer", function() {
+    it("should bind to UDP port 6370", function() {
+        // test here
+    });
+
+    it("should bind to UDP port 6371", function() {
+        // test here
+    });
+});
+
